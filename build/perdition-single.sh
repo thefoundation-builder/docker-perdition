5d9dd20f832b861124e7948788e2ee4c367041f4
Benjamin Freitag <gitlab.com@b1nch0.us.to>
2022-09-12 18:04:14 +0000

    Update build/perdition-single.sh

--- build/perdition-single.sh
+++ build/perdition-single.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "TARGETING $IMAPTARGET"
 logfilter() { cat ; } ;
-[[ "$DEBUG" = "true" ]] || logfilter() { grep -v -e "Connect:" -e " received=236 " -e "received=128 " -e "received=456 " -e 'server-secure=starttls status="ok"' ; } ;
+[[ "$DEBUG" = "true" ]] || logfilter() { grep -v -e "Connect:"  -e 'server-secure=starttls status="ok"' ; } ;
 
 [[ -z "$IMAPTARGET" ]] && echo "NO TARGET"
 [[ -z "$IMAPTARGET" ]] && exit 1
